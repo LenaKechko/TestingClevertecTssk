9b6fa348535084f63ac228f00b360ced6fd1c9b6
Elena Kechko <ekechko@gmail.com>
2023-10-27 22:22:58 +0300

    Commit init for testing mapper

---
+++ src/test/java/ru/clevertec/product/mapper/impl/ProductMapperImplTest.java
@@ -0,0 +1,25 @@
+package ru.clevertec.product.mapper.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductMapperImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void toProduct() {
+    }
+
+    @Test
+    void toInfoProductDto() {
+    }
+
+    @Test
+    void merge() {
+    }
+}
