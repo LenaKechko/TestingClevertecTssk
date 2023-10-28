0f45ebe99adf9b459de78cedb3b876aa936dd6e4
Elena Kechko <ekechko@gmail.com>
2023-10-28 19:52:52 +0300

    Commit init for testing repository

---
+++ src/test/java/ru/clevertec/product/repository/impl/InMemoryProductRepositoryTest.java
@@ -0,0 +1,49 @@
+package ru.clevertec.product.repository.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InMemoryProductRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void findAll() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void save() {
+        //given
+
+        //when
+
+        //then
+    }
+
+    @Test
+    void delete() {
+        //given
+
+        //when
+
+        //then
+    }
+}
